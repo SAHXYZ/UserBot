cdd2483691cdad46ab60e9d8332d30cab1261132
SAHXYZ <opsahalam97@gmail.com>
2025-12-01 05:38:50 +0530

    Create hi.py

---
+++ plugins/hi.py
@@ -0,0 +1,15 @@
+from pyrogram import Client, filters
+
+hi_art = (
+    "🌺✨           ✨🌺    🌺✨\n"
+    "🌺✨           ✨🌺    🌺✨\n"
+    "🌺✨ ✨  ✨🌺    🌺✨\n"
+    "🌺✨           ✨🌺    🌺✨\n"
+    "🌺✨           ✨🌺    🌺✨"
+)
+
+def init_hi(bot: Client):
+
+    @bot.on_message(filters.command("hi", prefixes=".") & filters.me)
+    async def hi_handler(_, message):
+        await message.reply_text(f"**{hi_art}**\n\n👋 **Hello SAH!**")
